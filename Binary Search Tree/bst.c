1efc06d52650870f7abd6219315459b2c0e305a3
Monish Ramalingam <155383938+monish-io@users.noreply.github.com>
2025-10-31 21:59:22 +0530

    Implement Binary Search Tree with traversal methods

---
+++ Binary Search Tree/bst.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct bst{
+    struct bst *left;
+    struct bst *right;
+    int roll;
+}TREE;
+void add_tree(TREE **,int);
+void pre_order(TREE *);
+void in_order(TREE *);
+void post_order(TREE *);
+int main(){
+    char op;
+    int n;
+    TREE *hptr=NULL;
+    do{
+      printf("Enter a serial num:\n");
+      scanf("%d",&n);
+	  add_tree(&hptr,n);
+      printf("Do you want to add another number?\n");
+      scanf(" %c",&op);
+    }while(op=='y');
+    printf("Printing Preorder.....\n");
+    pre_order(hptr);
+    printf("\nPrinting Inorder.....\n");
+    in_order(hptr);
+    printf("\nPrinting Postorder.....\n");
+    post_order(hptr);
+}
+void add_tree(TREE **ptr,int num){
+    if(*ptr==NULL){
+        *ptr=(TREE*)malloc(sizeof(TREE));
+        (*ptr)->left=NULL;
+        (*ptr)->right=NULL;
+        (*ptr)->roll=num;
+    }
+    else if((*ptr)->roll > num) add_tree(&(*ptr)->left,num);
+    else add_tree(&(*ptr)->right,num);
+}
+void pre_order(TREE *ptr){
+    if(ptr!=NULL){
+        printf("%d ",ptr->roll);
+        pre_order(ptr->left);
+        pre_order(ptr->right);
+    }
+}
+void in_order(TREE *ptr){
+    if(ptr){
+        in_order(ptr->left);
+        printf("%d ",ptr->roll);
+        in_order(ptr->right);
+    }
+}
+void post_order(TREE *ptr){
+    if(ptr){
+        post_order(ptr->left);
+        post_order(ptr->right);
+        printf("%d ",ptr->roll);
+    }
+}
